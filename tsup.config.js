9985b12074e09ed1c1c0f62d11028acdc14a1bcc
Madan Lal <iammadanlalmali@gmail.com>
2025-01-05 12:08:08 +0530

    chore: config for minified & release: v1.0.2

--- tsup.config.js
+++ tsup.config.js
@@ -1,12 +1,13 @@
 import { defineConfig } from "tsup";
 
 export default defineConfig({
-  entry: ["src/index.ts"],
-  dts: true,
-  outDir: "dist",
-  clean: true,
-  format: ["cjs", "esm"],
-  treeshake: true,
-  splitting: false,
-  cjsInterop: true,
+    entry: ["src/index.ts"],
+    dts: true,
+    outDir: "dist",
+    clean: true,
+    format: ["cjs", "esm"],
+    treeshake: true,
+    splitting: false,
+    cjsInterop: true,
+    minify: true,
 });
